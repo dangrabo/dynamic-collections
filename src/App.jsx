1aa3ffc98721f7dd2d702e30731e09dffd25cae7
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-10-07 13:48:31 -0700

    Add fonts, style and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Header from "./components/Header.jsx"
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <>
-    <Header />
-  </>
-  )
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function footer() {
+  return (
+    <footer>
+      <p>Authors: Daniel Grabowski, Alex Bolshakov</p>
+      <p>Green River College</p>
+    </footer>
+  );
+}
+
+export default footer;
