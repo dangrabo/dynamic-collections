155a2050ad0172ea60fd667104d2964243694895
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-10-07 13:35:49 -0700

    created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <img src="./ball.svg" alt="soccer-ball" id="header-img"/>
+            <h1>Soccer Player Stats</h1>
+        </header>
+    )
+}
+
+export default Header
